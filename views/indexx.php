f7f95a4a1679ee08031cf62d0e1d78c08a6a192a
Adler <adleraguilarlopez7@gmail.comv>
2025-05-23 12:28:10 -0500

    marco de mi formulario

--- views/indexx.php
+++ views/indexx.php
@@ -2,24 +2,27 @@
 <html lang="es">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Formulario de Contacto</title>
+    <title>Formulario con Marco</title>
 </head>
 <body>
 
     <h1>Formulario de Contacto</h1>
 
     <form action="#" method="post">
-        <label for="nombre">Nombre:</label><br>
-        <input type="text" id="nombre" name="nombre"><br><br>
+        <fieldset>
+            <legend>Datos del Usuario</legend>
 
-        <label for="correo">Correo:</label><br>
-        <input type="email" id="correo" name="correo"><br><br>
+            <label for="nombre">Nombre:</label><br>
+            <input type="text" id="nombre" name="nombre"><br><br>
 
-        <label for="comentarios">Comentarios:</label><br>
-        <textarea id="comentarios" name="comentarios"></textarea><br><br>
+            <label for="correo">Correo:</label><br>
+            <input type="email" id="correo" name="correo"><br><br>
 
-        <button type="submit">Enviar</button>
+            <label for="comentarios">Comentarios:</label><br>
+            <textarea id="comentarios" name="comentarios"></textarea><br><br>
+
+            <button type="submit">Enviar</button>
+        </fieldset>
     </form>
 
 </body>
